3044a78bc4df9e7feacd44201cdb608fda081d83
Richapatel22 <78552595+Richapatel22@users.noreply.github.com>
2021-02-04 21:16:52 +0530

    this code will swap two numbers

---
+++ swap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter two numbers for swapping");
+	scanf("%d,%d",&a,&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("numbers after swapping are %d %d,a,b);
+}
